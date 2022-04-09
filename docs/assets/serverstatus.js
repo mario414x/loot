9075f2f031de20ea583d3080cda54730d9d14b3c
mario414x <103279766+mario414x@users.noreply.github.com>
2022-04-09 15:24:20 +0300

    Update serverstatus.js

--- docs/assets/serverstatus.js
+++ docs/assets/serverstatus.js
@@ -7,8 +7,8 @@
         document.getElementById("serverInd").classList.add("text-green-600");
         document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, &ensp;&middot;&ensp;${data.players.online} se joaca</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} se joaca</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online &ensp;&middot;&ensp;${data.players.online} se joaca</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online ${data.players.online} se joaca</p>`;
         {% endif %}
     }
     else {
